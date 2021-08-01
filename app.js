ab9584004fa16e9860afe2e3710688a05b4046a6
aomini <rak3sh.shrestha@gmail.com>
2021-08-01 15:20:42 +0000

    :rocket:

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send(`
+		<h1>Lazy Pratim's club</h1>
+		`)
+})
+
+const port = process.env.port || 3000
+app.listen(port, () => {
+	console.log(`the process is running on port ${port}`)
+})
